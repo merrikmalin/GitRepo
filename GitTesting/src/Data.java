7d5cce4908210eca33189b5ec5441ec7977aa332
mmalin2 <mmalin2@GLC217E-PC.walsh.edu>
2019-11-26 10:51:24 -0500

    did some lower case

--- GitTesting/src/Data.java
+++ GitTesting/src/Data.java
@@ -3,7 +3,7 @@ public class Data {
 	public void displayData() {
 		System.out.println("TESTING DATA 1");
 		System.out.println("TESTING DATA 2");
-		System.out.println("TESTING DATA 3");
+		System.out.println("TESTING DATA 3 edited");
 	}
 
 }
