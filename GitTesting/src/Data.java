79cd6d08ff59cb00937aef7c5cd318a63c2640fb
mmalin2 <mmalin2@GLC217E-PC.walsh.edu>
2019-11-26 10:47:39 -0500

    Testing data 3 message

---
+++ GitTesting/src/Data.java
@@ -0,0 +1,9 @@
+public class Data {
+
+	public void displayData() {
+		System.out.println("TESTING DATA 1");
+		System.out.println("TESTING DATA 2");
+		System.out.println("TESTING DATA 3");
+	}
+
+}
---
+++ GitTesting/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		Data d1 = new Data();
+		d1.displayData();
+	}
+	
+}
